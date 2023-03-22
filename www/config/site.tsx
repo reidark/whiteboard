1aec2005bccd39ca9dde9d7f42ee642240dd212c
Victor Hugo Matias <victormatiasrei@gmail.com>
2023-03-22 02:06:18 -0300

    Update content path url

--- www/config/site.tsx
+++ www/config/site.tsx
@@ -6,6 +6,6 @@ export const siteLinks = {
   url: "https://whiteboard-xi-five.vercel.app",
   github: "https://github.com/reidark",
   repository: "https://github.com/reidark/whiteboard",
-  repositoryContent: "https://github.com/reidark/whiteboard/www/content",
+  repositoryContent: "https://github.com/reidark/whiteboard/tree/main/www",
   twitter: "https://twitter.com/notreidark",
 };
